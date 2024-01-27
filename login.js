5c762a35c799cb5c1319f1a5260776cbe4599e73
Austin-Creates <84189128+Austin-Creates@users.noreply.github.com>
2024-01-27 20:01:00 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,10 @@
+const loginBtn = document.querySelector("#login-btn");
+const registerBtn = document.querySelector("#register-btn");
+const loginIconBtn = document.querySelector("#login-icon-btn");
+const registerIconBtn = document.querySelector("#register-icon-btn");
+
+const loginForm = document.querySelector(".login-container");
+const registerForm = document.querySelector(".register-container");
+
+const fluid = document.querySelector("#fluid");
+
